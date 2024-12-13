1bdc83ac659b97da15b79a56811402c52d6d29de
Bazzzyan <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>
2024-12-13 10:49:26 +0200

    Release

---
+++ html.php
@@ -0,0 +1,17 @@
+<?php
+
+function html($tag, $content="", $a=array())
+{
+    $html="<".$tag;
+    foreach ($a as $param => $value)
+    {
+        $html.=" $param='$value'";
+    }
+    $html.=">";
+    if($content!="")
+    {
+        $html.=$content."</".$name.">";
+    }
+    return $html;
+}
+
